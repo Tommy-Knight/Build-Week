2a5b51411cc716c4d870e52d9819715e19b3a360
Saidev Lakkakula <lakkakula.saidev@gmail.com>
2021-04-08 02:42:30 +0200

    Homepage NavBar update1

--- assets/javaScript/HomePage.js
+++ assets/javaScript/HomePage.js
@@ -19,16 +19,17 @@ insertLogo()
 
 
 //  Get the container element
-/* var btnContainer = document.getElementById("HomePageNav");
+ var btnContainer = document.getElementById("HomePageNav");
 
 // Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("btn");
+var btns = btnContainer.getElementsByClassName("home-nav-btns");
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+    var current = document.getElementsByClassName("activateButton");
+    current[0].className = current[0].className.replace(" activateButton", "");
+    this.className += " activateButton";
   });
-} */
+} 
+
