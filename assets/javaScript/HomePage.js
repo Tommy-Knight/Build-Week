3d16965562c1fb70cd3afd7235c230991f59ad9c
Saidev Lakkakula <lakkakula.saidev@gmail.com>
2021-04-09 13:10:56 +0200

    HomePage with working  albums

--- assets/javaScript/HomePage.js
+++ assets/javaScript/HomePage.js
@@ -25,13 +25,13 @@ insertLogo()
 var btns = btnContainer.getElementsByClassName("home-nav-btns");
 
 // Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
+/* for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("activateButton");
     current[0].className = current[0].className.replace(" activateButton", "");
     this.className += " activateButton";
   });
-} 
+}  */
 
 /* function insertPlayIcon(){
 
@@ -49,9 +49,7 @@ for(let i=0; i<tileTitle.length;i++){
 
 function insertPlayIcon(){
 
-
 let albumImages = document.getElementsByClassName('rounded')
-
 for(let i=0; i<albumImages.length;i++){
     let iconDiv = document.createElement('div')
     iconDiv.className = 'newPlay'
@@ -59,5 +57,27 @@ for(let i=0; i<albumImages.length;i++){
     albumImages[i].after(iconDiv)
 }
 }
-
 insertPlayIcon()
+
+
+function HomePage(){
+  location.replace("HomePage.html")
+}
+
+function Podcast(){
+  location.replace("Podcast.html")
+}
+
+function Genres(){
+  location.replace("Genres.html")
+}
+
+function Newreleases(){
+  location.replace("NewReleases.html")
+}
+
+function Discover(){
+  location.replace("Discover.html")
+}
+
+
